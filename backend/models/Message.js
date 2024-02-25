74b8a357665e0ccfb3152f941c3ca63c9d7f397d
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-02-25 20:20:18 +0100

    Fix code's issue

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const messageSchema = mongoose.Schema({
     code: { 
-        type: Number, 
+        type: String, 
         unique: true 
     },
     message: String
