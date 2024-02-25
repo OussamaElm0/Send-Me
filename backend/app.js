97312bf945f93f48d3ec49ddb6437b8d087c2f0c
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-02-25 22:44:04 +0100

    Add cors

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,13 @@ const express = require('express')
 const app = express()
 const port = process.env.B_PORT || 4000
 const homeRouter = require('./routes/homeRouter')
+const cors = require('cors')
 require('./config/database')
 
+app.use(cors())
 app.use(express.json())
 app.use('/', homeRouter)
+
 
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`))
